9aef4f85209bbb8aeb985da5e351093eff12e229
dazenpc <121750352+dazenpc@users.noreply.github.com>
2025-01-21 22:29:32 +0530

    feat: create operate method

--- scripts.js
+++ scripts.js
@@ -13,3 +13,24 @@ function subtract(num1, num2){
 function divide(num1, num2){
     return num1/num2
 }
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator){
+    switch(operator){
+        case "+":
+            add(num1,num2)
+            break
+        case "-":
+            subtract(num1,num2)
+            break
+        case "*":
+            multiply(num1, num2)
+            break
+        case "/":
+            divide(num1, num2)
+            break
+    }
+}
